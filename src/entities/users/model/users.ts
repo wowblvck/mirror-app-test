40332fd27bf8ea8c73f77ffba8a58eb88d74527b
Indar Basto <indar.basto@gmail.com>
2023-11-29 09:02:34 +0300

    packages: update effector

--- src/entities/users/model/users.ts
+++ src/entities/users/model/users.ts
@@ -9,11 +9,16 @@ const getUsersListFx = createEffect(async () => {
 
 const UsersGate = createGate();
 
-const $users = restore(getUsersListFx.doneData, []);
+const $users = restore(getUsersListFx, []);
 
 const $loading = getUsersListFx.pending;
 
-const $filteredUsers = createStore<User[]>([]).on($users, (_, payload) => payload);
+const $filteredUsers = createStore<User[]>([]);
+
+sample({
+  clock: $users,
+  target: $filteredUsers,
+});
 
 const selectUser = createEvent<User>();
 
